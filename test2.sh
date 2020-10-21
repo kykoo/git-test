962902921f1f31beb493c18efc5c84e51c7d15cd
kykoo <dr.ki.koo@gmail.com>
2020-10-21 07:19:33 +0100

    test2

---
+++ test2.sh
@@ -0,0 +1,13 @@
+modification !
+
+#! /bin/sh
+
+env >>/tmp/test.log
+file "/sys${DEVPATH}" >>/tmp/test.log
+
+echo "${ACTION}"
+echo "/sys${DEVPATH}"
+
+if [ "${ACTION}" = add -a -d "/sys${DEVPATH}" ]; then
+echo "add ${DEVPATH}" >>/tmp/test.log
+fi
